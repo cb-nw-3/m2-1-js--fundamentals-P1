5d7612dc727317ff5085c62b1dbae733aebb4f2d
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-22 21:13:08 -0400

    completed 5-4.5

--- workshop/5-control-flow/5-exercise-4.5.js
+++ workshop/5-control-flow/5-exercise-4.5.js
@@ -15,3 +15,10 @@
 // ...
 
 // many other possibilities here.
+for (let number = 0; number <= 15; number++) {
+  if (number % 2 === 0) {
+    console.log(number, "is even");
+  } else {
+    console.log(number, "is odd");
+  }
+}
