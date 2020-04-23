47b4ba18c63282795957fd995c83333362c3fd62
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-04-22 21:45:15 -0400

    answered #8 and #9

--- workshop/5-control-flow/5-exercise-9.js
+++ workshop/5-control-flow/5-exercise-9.js
@@ -17,3 +17,36 @@
 // _#_#_#_#
 // #_#_#_#_
 // _#_#_#_#
+
+
+//solution
+
+for(let x=1; x<8; x++) {
+    // line needs to be declared inside the block, if its outside, then when the loop ends it saves the added characters instead of creating a new line
+    let line='';
+
+    //if the first x is odd, start with a # character
+    if(x % 2 != 0) {
+        line += '#';
+        // for the remaining 7 characters, alternate between _ and #
+        for(let y=1; y<8; y++) {
+            if(y % 2 != 0) {
+                line += "_";
+            } else {
+                line += "#";
+            }
+        }
+    //if the first x is even, start with a _ character
+    } else {
+        line += '_';
+        // for the remaining 7 characters, alternate between # and _
+        for(let y=1; y<8; y++) {
+            if(y % 2 != 0) {
+                line += "#";
+            } else {
+                line += "_";
+            }
+        }
+    }
+    console.log(line);
+}
